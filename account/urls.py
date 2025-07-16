bb55f16aef778858e1dd2058cefcdb56a9465502
Dilshad <dilshadalicp28@gmail.com>
2025-07-16 21:06:02 +0530

    Fix: Corrected class name from register to Register in account.urls

--- account/urls.py
+++ account/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
 
-    path('register/',views.register.as_view()),
-    path('login/',views.logg.as_view()),
+    path('register/',views.Register.as_view()),
+    path('login/',views.Login.as_view()),
     path('self/',views.SelfUserView.as_view()),
     # path('profile/',views.student)
 ]
