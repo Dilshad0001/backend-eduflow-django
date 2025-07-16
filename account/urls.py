676945685f6fd817dde848b2642f5001f45081d6
Dilshad <dilshadalicp28@gmail.com>
2025-07-16 21:45:10 +0530

    chnaged

--- account/urls.py
+++ account/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
 
     path('register/',views.Register.as_view()),
     path('login/',views.Login.as_view()),
+    path('',views.Login.as_view()),
     path('self/',views.SelfUserView.as_view()),
     # path('profile/',views.student)
 ]
--- eduflow/urls.py
+++ eduflow/urls.py
@@ -10,6 +10,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
+    path('', include('account.urls')),
+
     path('student/', include('student.urls')),
     path('adminuser/', include('adminuser.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
